9c2c54b2db2c390c0df6b03e957a9cc9b40b928b
Lila Fallet <lilafallet@gmail.com>
2020-01-23 16:12:47 +0100

    ex

--- ex_state_machine.c
+++ ex_state_machine.c
@@ -3,17 +3,21 @@
 //				1) etat de pointeurs sur fonction
 //				2) etat des regles de transitions
 
-int entry_state(void); //entree
-int foo_state(void); //pousser
-int bar_state(void); //bloquer
-int exit_state(void); //quitter
+int		entry_state(void); //entree
+int 	foo_state(void); //pousser
+int 	bar_state(void); //bloquer
+int 	exit_state(void); //quitter
 
 /* array and enum below must be in sync! */
-int (* state[])(void) = { entry_state, foo_state, bar_state, exit_state};
-enum state_codes { entry, foo, bar, end};
 
-enum ret_codes { ok, fail, repeat};
-struct transition {
+int		(* state[])(void) = {entry_state, foo_state, bar_state, exit_state};
+//etat du pointeur sur fonction
+enum	state_codes {entry, foo, bar, end};
+// etat des etats de transition
+
+enum 	ret_codes {ok, fail, repeat};
+typedef	struct	transition 
+{
     enum state_codes src_state;
     enum ret_codes   ret_code;
     enum state_codes dst_state;
@@ -32,18 +36,19 @@ struct transition state_transitions[] = {
 #define EXIT_STATE end
 #define ENTRY_STATE entry
 
-int main(int argc, char *argv[]) {
-    enum state_codes cur_state = ENTRY_STATE;
-    enum ret_codes rc;
-    int (* state_fun)(void);
+int main(int argc, char *argv[])
+{
+    enum	state_codes cur_state = ENTRY_STATE;
+    enum 	ret_codes rc;
+    int		(* state_fun)(void);
 
-    for (;;) {
+    for (;;) 
+	{
         state_fun = state[cur_state];
         rc = state_fun();
         if (EXIT_STATE == cur_state)
             break;
         cur_state = lookup_transitions(cur_state, rc);
     }
-
     return EXIT_SUCCESS;
 }
