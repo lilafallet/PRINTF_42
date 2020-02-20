d68853ee288bc2a227edb0071ff120a826a778be
Lila Fallet <lfallet@student.42.fr>
2020-02-19 16:09:59 -0800

    [REFACTO] conversion_csp

--- sources/conversion/conversion_csp.c
+++ sources/conversion/conversion_csp.c
@@ -56,73 +56,71 @@ char	*s_conv(char *str, t_option *option)
 	return (new_str);
 }
 
+char	*p_is_zero(t_option *option, t_option *cpy_option)
+{
+	char	*str_pzero;
+	const char	*pre_string = "0x";
+
+	option->width = cpy_option->width - 2;
+	if (cpy_option->width != 0)
+	{
+		str_pzero = (char *)malloc(sizeof(char) * (cpy_option->width + 2));
+		if (str_pzero == NULL)
+		{
+			option->len_conversion = 0;
+			return (NULL);
+		}
+		str_pzero = strjoin_p_conversion(str_pzero, cpy_option, option, "\0");
+		return (str_pzero);
+	}
+	str_pzero = (char *)malloc(sizeof(char) * (2 + 1));
+	if (str_pzero == NULL)
+	{
+		option->len_conversion = 0;
+		return (NULL);
+	}
+	option->len_conversion = 2;
+	return (ft_strdup(pre_string));
+} 
+
+void	get_p_width(t_option *option, long len)
+{
+	if (option->width > len && option->width > option->precision)
+	{
+		if (len > option->precision)
+			option->width = option->width > (len + 2) ?
+				option->width - (len + 2) : 0;
+		else
+			option->width = option->width - (option->precision + 2);
+	}
+	else
+		option->width = 0;
+}	
+
 char	*p_conv(unsigned long p, t_option *option)
 {
 	char		*new_str;
 	char		*number;
 	t_option	cpy_option;
-	size_t		len;
+	long		len;
 
 	cpy_option.flag = option->flag;
 	cpy_option.precision = option->precision;
 	cpy_option.width = option->width;
 	number = ft_ultoa_base(p, 16);
-	len = ft_strlen(number);
-	if (option->width > (long)len && option->width > option->precision)
-	{
-		if ((long)len > option->precision)
-		{
-			if (option->width > ((long)len + 2))
-				option->width = cpy_option.width - ((long)len + 2);
-			else
-				option->width = 0;
-			option->precision = 0;
-		}
-		else if ((long)len < option->precision)
-		{
-			option->width = cpy_option.width - (option->precision + 2);
-			option->precision = cpy_option.precision - (long)len;
-		}
-	}
-	else if (option->width < (long)len)
-	{
-		option->width = 0;
-		option->precision = cpy_option.precision - (long)len;
-		if (option->precision <= (long)len)
-			option->precision = 0;
-	}
+	len = (long)ft_strlen(number);
+	get_p_width(option, len);
+	option->precision = option->precision <= len ? 0 : option->precision - len;
 	if (option->precision == 0 && p == 0 && len == 1 && option->flag & MOD_DOT)
-	{	
-		option->width = cpy_option.width - 2;
-		if (cpy_option.width != 0)
-		{
-			new_str = (char *)malloc(sizeof(char) * (cpy_option.width + 2));
-			if (new_str == NULL)
-			{
-				option->len_conversion = 0;
-				return (NULL);
-			}
-			new_str = strjoin_p_conversion(new_str, &cpy_option, option, "\0");
-			return (new_str);
-		}
-		new_str = (char *)malloc(sizeof(char) * (2 + 1));
-		if (new_str == NULL)
-		{
-			option->len_conversion = 0;
-			return (NULL);
-		}
-		new_str[0] = '0';
-		new_str[1] = 'x';
-		new_str[2] = '\0';
-		option->len_conversion = 2;
+	{
+		free(number);
+		new_str = p_is_zero(option, &cpy_option);
 		return (new_str);
-	}	
-	else
-		new_str = (char *)malloc(sizeof(char) * (option->precision +
-												option->width + 2 + 1));
+	}
+	new_str = (char *)malloc(sizeof(char) * (option->precision +
+				option->width + 2 + 1));
 	if (new_str != NULL)
-		new_str = strjoin_p_conversion(new_str, &cpy_option, option,
-													number);
+		new_str = strjoin_p_conversion(new_str, &cpy_option, option, number);
 	free(number);
 	return (new_str);
 }
